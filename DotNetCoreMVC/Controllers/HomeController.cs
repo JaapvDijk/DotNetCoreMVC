8003891fe4df7a54ce28d0f56bf54289d22dfd91
Jaap <dijkvanjaap@hotmail.com>
2021-12-09 15:39:19 +0100

    Add project files.

---
+++ DotNetCoreMVC/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using DotNetCoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly List<Laptop> _laptops = new()
+        {
+            new Laptop() { Name = "Lenovo Ideapad" },
+            new Laptop() { Name = "DELL XPS" },
+            new Laptop() { Name = "HP Omen" }
+        };
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(string? search)
+        {
+            LaptopsViewModel result = new();
+            result.LaptopList = _laptops;
+
+            //TODO: search remains null in result.search
+            //TODO: replace search with result.search
+            if (search != null)
+            {
+                return View(result.GetByName(search));
+            }
+
+            return View(result);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DotNetCoreMVC/Models/Laptop.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreMVC.Models
+{
+    public interface IDevice
+    {
+        string Name { get; set; }
+    }
+
+    public interface ILaptop : IDevice
+    {
+        string? CPU { get; set; }
+        int? Storage { get; set; }
+    }
+
+    public class Laptop : ILaptop
+    {
+        public string Name { get; set; }
+        public string? CPU { get; set; }
+        public int? Storage { get; set; }
+    }
+
+    public class LaptopsViewModel : PageModel
+    {
+        public IEnumerable<Laptop> LaptopList { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string search { get; set; }
+        public LaptopsViewModel GetByName(string name)
+        {
+            LaptopList = LaptopList.Where(x => x.Name.ToLower().Contains(name));
+            return this;
+        }
+    }
+}
