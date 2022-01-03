c344eb3ab42d40390322c2c0f59fcd70f2651bab
Jaap <dijkvanjaap@hotmail.com>
2022-01-03 18:32:15 +0100

    test project

---
+++ DotNetLearningTests/SampleTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DotNetLearningTests
+{
+    public class SampleTest
+    {
+        [Fact]
+        public void SampleTest1()
+        {
+            Assert.True(true);
+        }
+    }
+}
