b9470504bec1dd3c7a44d45d2dc6a68479874091
Jaap <dijkvanjaap@hotmail.com>
2022-01-06 11:43:05 +0100

    temp

--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -45,24 +45,24 @@ namespace IdentityServer
         {
             try
             {
-                if (env.IsDevelopment())
-                {
-                    app.UseDeveloperExceptionPage();
-                }
-                else
-                {
-                    app.UseExceptionHandler("/Error");
-                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                    //app.UseHsts();
-                }
+                //if (env.IsDevelopment())
+                //{
+                //    app.UseDeveloperExceptionPage();
+                //}
+                //else
+                //{
+                //    app.UseExceptionHandler("/Error");
+                //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                //    app.UseHsts();
+                //}
 
                 //app.UseHttpsRedirection();
-                app.UseStaticFiles();
+                //app.UseStaticFiles();
 
                 app.UseRouting();
 
-                app.UseIdentityServer();
-                app.UseAuthorization();
+                //app.UseIdentityServer();
+                //app.UseAuthorization();
 
                 app.UseEndpoints(endpoints =>
                 {
